49df8203a4456fb647fa18c82cb2187a3c776e52
Adrian Chwiłkowski <adrianchwilkowski@gmail.com>
2023-07-10 13:17:35 +0200

    corrected displaying in index page data (changed studentId to student name and surname, etc.)

--- ProjektDziennik/Pages/Index.cshtml.cs
+++ ProjektDziennik/Pages/Index.cshtml.cs
@@ -26,30 +26,86 @@ namespace ProjektDziennik.Pages
         }
         public IList<Mark> Mark { get; set; } = default!;
         public IList<Mark> TeacherMark { get; set; } = default!;
+        public List<User> UserInfo { get; set; } = default!;
+        public List<User> UserInfo2 { get; set; } = default!;
+        public List<string> Roles = new List<string>();
+        public string name { get; set; }
+        public string surname { get; set; }
+
         public async Task OnGetAsync()
         {
             if (_httpContextAccessor.HttpContext.User.IsInRole("Admin"))
             {
+                name = "Student name";
+                surname = "Student surname";
+                Roles.Add("Admin");
+                Roles.Add("Student");
+                Roles.Add("Teacher");
+                UserInfo = new List<User>();
+                UserInfo2 = new List<User>();
                 Mark = await _context.Marks
                     .Include(s => s.Teacher)
                     .ToListAsync();
-                TeacherMark = await _context.Marks
-                    .Include(s => s.Student)
-                    .ToListAsync();
+
+                User StudentAdd;
+                User TeacherAdd;
+                for (int i = 0; i < Mark.Count; i++)
+                {
+                    StudentAdd = await _context.Users
+                    .Where(u => u.Id == Mark[i].StudentId)
+                    .FirstOrDefaultAsync();
+                    UserInfo.Add(StudentAdd);
+
+                    TeacherAdd = await _context.Users
+                    .Where(u => u.Id == Mark[i].TeacherId)
+                    .FirstOrDefaultAsync();
+                    UserInfo2.Add(TeacherAdd);
+                }
+
             }
-            else
+            else if (_httpContextAccessor.HttpContext.User.IsInRole("Student"))
             {
+                name = "Teacher name";
+                surname = "Teacher surname";
+                UserInfo = new List<User>();
                 var userId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+                Roles.Add("Student");
                 Mark = await _context.Marks
                 .Where(e => e.StudentId == userId)
-                .Include(s => s.Teacher)
+                //.Include(s => s.Teacher)
                 .ToListAsync();
-                TeacherMark = await _context.Marks
-                .Where(e => e.TeacherId == userId)
-                .Include(s => s.Student)
-                .ToListAsync();
+                User TeacherAdd;
+                for (int i = 0; i < Mark.Count; i++)
+                {
+                    TeacherAdd = await _context.Users
+                    .Where(u => u.Id == Mark[i].TeacherId)
+                    .FirstOrDefaultAsync();
+                    UserInfo.Add(TeacherAdd);
+                }
+
             }
-            
+            else if (_httpContextAccessor.HttpContext.User.IsInRole("Teacher"))
+            {
+                name = "Student name";
+                surname = "Student surname";
+                UserInfo = new List<User>();
+                var userId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+                Roles.Add("Teacher");
+                Mark = await _context.Marks
+                .Where(e => e.TeacherId == userId)
+                //.Include(s => s.Teacher)
+                .ToListAsync();
+                User StudentAdd;
+                for (int i = 0; i < Mark.Count; i++)
+                {
+                    StudentAdd = await _context.Users
+                    .Where(u => u.Id == Mark[i].StudentId)
+                    .FirstOrDefaultAsync();
+                    UserInfo.Add(StudentAdd);
+                }
+
+            }
+
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,17 @@
+# ProjektDziennik
+The project imitates an electronic gradebook. It has few built-in accounts with different roles: student, teacher and admin. To run it, enter in NuGet package console following commands:
+<br>Install-Package Microsoft.EntityFrameworkCore.Design
+<br>Install-Package Microsoft.EntityFrameworkCore.SqlServer
+<br>Install-Package Microsoft.EntityFrameworkCore.Tools<br>
+Update-database<br>
+
+The first thing that appears after running is a login page. If you login with correct data, you will be redirected to page with list of marks connected to your account.<br>
+
+Here are few accounts login data:<br>
+login: superadmin password: 123Pa$$word. role: admin<br>
+login: Teacher1 password: Qazxsw%531 role: teacher<br>
+login: Teacher2 password: Qazxsw%531 role: teacher<br>
+login: Teacher3 password: Qazxsw%531 role: teacher<br>
+login: student password: Qazxsw%531 role: student<br>
+login: student2 password: Qazxsw%531 role: student<br>
+and according pattern up to student6
